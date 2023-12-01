89886efc5defc6d58bb76426078dbefb8057332d
julian94 <langlo94@gmail.com>
2023-12-01 12:09:14 +0100

    Day one done!

---
+++ 2023-1a/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var data = File.ReadAllLines("input.txt");
+
+var sum = 0;
+
+for (int h = 0; h < data.Length; h++)
+{
+    var chars = data[h];
+    var first = 0;
+    var second = 0;
+    for (int i = 0; i < chars.Length; i++)
+    {
+        if (int.TryParse(chars[i].ToString(), out var result))
+        {
+            first = result;
+            break;
+        }
+    }
+    for (int i = chars.Length -1; i >= 0; i--)
+    {
+        if (int.TryParse(chars[i].ToString(), out var result))
+        {
+            second = result;
+            break;
+        }
+    }
+
+    Console.WriteLine($"{first}{second}: {data[h]}");
+    var number = int.Parse($"{first}{second}");
+    sum += number;
+}
+
+Console.WriteLine($"The sum is: {sum}");
+// The sum is: 54951
---
+++ 2023-1b/Program.cs
@@ -0,0 +1,66 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var data = File.ReadAllLines("input.txt");
+
+var sum = 0;
+
+/* One
+ * Two
+ * Three
+ * Four
+ * Five
+ * Six
+ * Seven
+ * Eight
+ * Nine
+ * NOT zero
+ * 
+ * NOTE:
+ * twone
+ * eightwo
+ */
+
+for (int h = 0; h < data.Length; h++)
+{
+    var chars = ReplaceStringNumbers(data[h]);
+    var first = 0;
+    var second = 0;
+    for (int i = 0; i < chars.Length; i++)
+    {
+        if (int.TryParse(chars[i].ToString(), out var result))
+        {
+            first = result;
+            break;
+        }
+    }
+    for (int i = chars.Length - 1; i >= 0; i--)
+    {
+        if (int.TryParse(chars[i].ToString(), out var result))
+        {
+            second = result;
+            break;
+        }
+    }
+
+    Console.WriteLine($"{first}{second}: {data[h]}");
+    var number = int.Parse($"{first}{second}");
+    sum += number;
+}
+
+Console.WriteLine($"The sum is: {sum}");
+// The sum is: 55218
+
+string ReplaceStringNumbers(string input) => input
+    .Replace("twone",   "21")
+    .Replace("eightwo", "82")
+    .Replace("oneight", "18")
+    .Replace("one",     "1")
+    .Replace("two",     "2")
+    .Replace("three",   "3")
+    .Replace("four",    "4")
+    .Replace("five",    "5")
+    .Replace("six",     "6")
+    .Replace("seven",   "7")
+    .Replace("eight",   "8")
+    .Replace("nine",    "9");
